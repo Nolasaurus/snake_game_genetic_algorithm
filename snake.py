8fa6d040a193da1d553713e46b6d14e68b0c1a14
Nolan <meyer09@gmail.com>
2024-01-15 19:46:23 -0600

    add snake self-collision detection

--- snake.py
+++ snake.py
@@ -7,7 +7,7 @@ class Snake:
         x_mid = grid_size[0] // 2
         y_mid = grid_size[1] // 2
 
-        self.head_direction = 'UP'
+        self.head_direction = 'DOWN'
         self.snake = LinkedList()
 
         # Add initial snake segments; top-left of screen is x=0, y=0
@@ -19,11 +19,9 @@ class Snake:
         return self.snake.to_list()
 
     def move(self, game_grid_object):
-        '''
-        runs once per gametick
-        '''
         head_x, head_y = self.snake_body()[0]
-    
+
+        # Movement logic
         if self.head_direction == 'UP':
             head_y -= 1
         elif self.head_direction == 'DOWN':
@@ -35,13 +33,19 @@ class Snake:
 
         next_head = (head_x, head_y)
 
-        if head_x < 0 or head_y < 0:
-            sys.exit('Snake hit a wall')
-        if head_x >= self.grid_size[0] or head_y >= self.grid_size[1]:
+        # Wall collision logic
+        if head_x < 0 or head_y < 0 or head_x >= self.grid_size[0] or head_y >= self.grid_size[1]:
             sys.exit('Snake hit a wall')
 
         self.snake.insert_at_beginning(next_head)
-        if (head_x, head_y) in game_grid_object.food:
-            pass
+
+        # Check if the snake ate food
+        snake_ate_food = next_head in game_grid_object.food
+        if snake_ate_food:
+            game_grid_object.spawn_food()
         else:
             self.snake.remove_last_node()
+
+        # Self-collision check
+        if next_head in self.snake_body()[1:]:
+            sys.exit('Tried to eat own body')
